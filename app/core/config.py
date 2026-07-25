ea2f55a5aa6726ceaeff356898cb370b969c4727
priyadharshiniannadurai413 <priyadharshiniannadurai413@gmail.com>
2026-07-25 17:01:31 +0530

    deploy please

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,6 @@ class Settings:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
     MONGODB_URL = os.getenv("MONGODB_URL")
-    MONGO_URL = os.getenv("MONGODB_URL") or os.getenv("MONGO_URL")
     DB_NAME = os.getenv("DB_NAME", "Chatbot")
 
 settings = Settings()
